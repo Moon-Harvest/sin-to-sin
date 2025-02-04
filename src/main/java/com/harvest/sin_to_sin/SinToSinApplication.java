dd9ce3c68ab5b07b71e7e46b25bcb20d98669683
deroy <deroy@barrier.barrierland>
2025-02-04 17:03:58 -0600

    First upload

---
+++ src/main/java/com/harvest/sin_to_sin/SinToSinApplication.java
@@ -0,0 +1,13 @@
+package com.harvest.sin_to_sin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SinToSinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SinToSinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/harvest/sin_to_sin/SinToSinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.harvest.sin_to_sin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SinToSinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
